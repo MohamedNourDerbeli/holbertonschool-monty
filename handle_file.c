4b5cfa5b9029c9371539f5f067170dcbbd8ea924
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-03 19:44:04 +0100

    handel the file and tokanayzet to get the op

---
+++ handle_file.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+int file(char *file, stack_t **stack)
+{
+    FILE *of;
+    size_t len, ret;
+    unsigned int line_number = 1;
+    char *line = NULL;
+    char *cmd, *delim = "\n \t\r";
+    if (!file)
+    {
+        fprintf(stderr, FILE_NOT_OPEN, file);
+        exit(EXIT_FAILURE);
+    }
+    of = fopen(file, "r");
+    if (of == NULL)
+    {
+        fprintf(stderr, FILE_NOT_OPEN, file);
+        exit(EXIT_FAILURE);
+    }
+    while ((ret = getline(&line, &len, of) != -1))
+    {
+        cmd = strtok(line, delim);
+        line_number++;
+        if (cmd)
+        {
+            
+        }
+        
+    }
+    free(line);
+    fclose(of);
+    return (0);
+}
