9abc9d56a42e083a4df17c75edcf58353998b0fd
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-05 23:00:32 +0100

    handel the file and tokanayzet to get the op

--- handle_file.c
+++ handle_file.c
@@ -1,31 +1,37 @@
 #include "monty.h"
+/**
+ * file - Reads and executes commands from a file.
+ * @file: File name to be read.
+ * @stack: Pointer to the top of the stack.
+ * Return: EXIT_SUCCESS or EXIT_FAILURE.
+ **/
 int file(char *file, stack_t **stack)
 {
-    FILE *of;
-    size_t len, ret;
-    unsigned int line_number = 0;
-    char *line = NULL;
-    char *cmd, *delim = "\n \t\r";
+	FILE *of;
+	size_t len, ret;
+	unsigned int line_number = 0;
+	char *line = NULL;
+	char *cmd, *delim = "\n \t\r";
 
-    if (!file)
-    {
-        fprintf(stderr, FILE_NOT_OPEN, file);
-        exit(EXIT_FAILURE);
-    }
-    of = fopen(file, "r");
-    if (of == NULL)
-    {
-        fprintf(stderr, FILE_NOT_OPEN, file);
-        exit(EXIT_FAILURE);
-    }
-    while ((ret = getline(&line, &len, of) != -1))
-    {
-        cmd = strtok(line, delim);
-        line_number++;
-        if (cmd)
-            opcode(stack, cmd, line_number);
-    }
-    free(line);
-    fclose(of);
-    return (EXIT_FAILURE);
+	if (!file)
+	{
+		fprintf(stderr, FILE_NOT_OPEN, file);
+		exit(EXIT_FAILURE);
+	}
+	of = fopen(file, "r");
+	if (of == NULL)
+	{
+		fprintf(stderr, FILE_NOT_OPEN, file);
+		exit(EXIT_FAILURE);
+	}
+	while ((ret = getline(&line, &len, of) != -1))
+	{
+		cmd = strtok(line, delim);
+		line_number++;
+		if (cmd)
+			opcode(stack, cmd, line_number);
+	}
+	free(line);
+	fclose(of);
+	return (EXIT_FAILURE);
 }
