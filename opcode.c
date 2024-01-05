8f054f5a2852ed22cf41cb10cf3b5e4704034e9d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-05 23:04:35 +0100

    add comment

--- opcode.c
+++ opcode.c
@@ -1,24 +1,29 @@
 #include "monty.h"
-
+/**
+ * opcode - Execute the corresponding operation based on the opcode.
+ * @stack: Double pointer to the top of the stack.
+ * @cmd: The opcode to be executed.
+ * @line_number: Line number in the Monty bytecode file.
+ **/
 void opcode(stack_t **stack, char *cmd, unsigned int line_number)
 {
-    int i = 0;
-    instruction_t ops[] = {
-        {"push", _push},
-        {"pall", _pall},
-        {NULL, NULL}};
+	int i = 0;
+	instruction_t ops[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}};
 
-    for (i = 0; ops[i].opcode; i++)
-    {
-        if (strcmp(cmd, ops[i].opcode) == 0)
-        {
-            ops[i].f(stack, line_number);
-            return;
-        }
-    }
-    if (strlen(cmd) != 0 && cmd[0] != '#')
-    {
-        fprintf(stderr, INVAILD_INSTRUCTION, line_number, cmd);
-        exit(EXIT_FAILURE);
-    }
+	for (i = 0; ops[i].opcode; i++)
+	{
+		if (strcmp(cmd, ops[i].opcode) == 0)
+		{
+			ops[i].f(stack, line_number);
+			return;
+		}
+	}
+	if (strlen(cmd) != 0 && cmd[0] != '#')
+	{
+		fprintf(stderr, INVAILD_INSTRUCTION, line_number, cmd);
+		exit(EXIT_FAILURE);
+	}
 }
