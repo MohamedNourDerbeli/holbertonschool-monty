d6fb3244841dace625f3232a0b44f647ea4d314f
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-05 23:02:02 +0100

    monty file

--- monty.c
+++ monty.c
@@ -1,14 +1,19 @@
 #include "monty.h"
-
+/**
+ * main - Entry point for the Monty bytecode interpreter.
+ * @argc: Number of command line arguments.
+ * @argv: Array of strings containing the command line arguments.
+ * Return: EXIT_SUCCESS or EXIT_FAILURE.
+ **/
 void main(int argc, char **argv)
 {
-    stack_t *stack = NULL;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, ARGUMENT_NOT_CORRECT);
-        exit(EXIT_FAILURE);
-    }
-    file(argv[1], &stack);
-    exit(EXIT_SUCCESS);
+	if (argc != 2)
+	{
+		fprintf(stderr, ARGUMENT_NOT_CORRECT);
+		exit(EXIT_FAILURE);
+	}
+	file(argv[1], &stack);
+	exit(EXIT_SUCCESS);
 }
