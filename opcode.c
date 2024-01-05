dc823962e0cf3724b6cc938c2efdc7497a91e569
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-05 22:56:37 +0100

    handle same error

--- opcode.c
+++ opcode.c
@@ -6,20 +6,17 @@ void opcode(stack_t **stack, char *cmd, unsigned int line_number)
     instruction_t ops[] = {
         {"push", _push},
         {"pall", _pall},
-        {"pint", _pint},
-        {"pop", _pop},
-        {"swap", _swap},
         {NULL, NULL}};
 
     for (i = 0; ops[i].opcode; i++)
     {
-        if (strcmp(ops[i].opcode, cmd) == 0)
+        if (strcmp(cmd, ops[i].opcode) == 0)
         {
             ops[i].f(stack, line_number);
             return;
         }
     }
-    if (strlen(cmd)!=0)
+    if (strlen(cmd) != 0 && cmd[0] != '#')
     {
         fprintf(stderr, INVAILD_INSTRUCTION, line_number, cmd);
         exit(EXIT_FAILURE);
