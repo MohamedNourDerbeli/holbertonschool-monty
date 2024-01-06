415120b189bc7b02661d389884fa666109eea5a9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 08:54:53 +0100

    header

--- monty.h
+++ monty.h
@@ -1,8 +1,15 @@
 #ifndef MONTY
 #define MONTY
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
 #define ARGUMENT_NOT_CORRECT "USAGE: monty file\n"
 #define FILE_NOT_OPEN "Error: Can't open file %s\n<file>"
 #define INVAILD_INSTRUCTION "L%u: unknown instruction %s\n"
