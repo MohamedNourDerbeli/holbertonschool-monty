2a51734cdcf0ba8e33d800a4e93face82446de49
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 16:14:29 +0100

    fix malloc

--- handle_int.c
+++ handle_int.c
@@ -9,35 +9,35 @@
  **/
 int _strtol(char *op, unsigned int line_number)
 {
-    char *ptr;
-    long ret;
+	char *ptr;
+	long ret;
 
-    errno = 0;
+	errno = 0;
 
-    if (op[0] == '\0')
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (op[0] == '\0')
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    ret = strtol(op, &ptr, 10);
+	ret = strtol(op, &ptr, 10);
 
-    if ((errno == ERANGE && (ret == LONG_MAX || ret == LONG_MIN)))
-    {
-        fprintf(stderr, "L%d: number out of range for long\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    else if (errno != 0 && ret == 0)
-    {
-        perror("strtol");
-        exit(EXIT_FAILURE);
-    }
+	if ((errno == ERANGE && (ret == LONG_MAX || ret == LONG_MIN)))
+	{
+		fprintf(stderr, "L%d: number out of range for long\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else if (errno != 0 && ret == 0)
+	{
+		perror("strtol");
+		exit(EXIT_FAILURE);
+	}
 
-    if (*ptr != '\0')
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*ptr != '\0')
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    return (ret);
+	return (ret);
 }
