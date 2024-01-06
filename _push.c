76b8a8f833c37ddeddb6c4dd9a4c55dd1373a172
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 14:56:31 +0100

    push fun

--- _push.c
+++ _push.c
@@ -8,15 +8,15 @@ void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node = malloc(sizeof(stack_t));
 	char *op;
-	char *DELIMS = "\n \t\r";
+	char *delims = "\n \t\r";
 	int num;
 
 	if (node == NULL)
 	{
-		printf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	op = strtok(NULL, DELIMS);
+	op = strtok(NULL, delims);
 	if (stack == NULL || op == NULL)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
@@ -29,19 +29,4 @@ void _push(stack_t **stack, unsigned int line_number)
 	if (node->next != NULL)
 		(node->next)->prev = node;
 	*stack = node;
-}
-/**
- * _pall - Prints all the values on the stack.
- * @stack: Double pointer to the top of the stack.
- * @line_number: Line number in the Monty bytecode file
- **/
-void _pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
-{
-	stack_t *node = *stack;
-
-	while (node != NULL)
-	{
-		printf("%d\n", node->n);
-		node = node->next;
-	}
 }
