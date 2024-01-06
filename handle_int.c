1a6c59ce632b57ebab81e18927c8502342adbe18
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 17:58:06 +0100

    fix malloc

--- handle_int.c
+++ handle_int.c
@@ -16,6 +16,10 @@ int _strtol(char *op, unsigned int line_number)
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	if (op[0] != '\0')
+		if (!isdigit(op[0]) && *ptr != '\0')
+		{
+			return (-ret);
+		}
 	return (ret);
 }
