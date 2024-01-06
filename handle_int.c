05e3751fa4218514d01e6fb77f87297365f5e702
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 09:00:50 +0100

    split op

--- handle_int.c
+++ handle_int.c
@@ -19,5 +19,14 @@ int _strtol(char *op, unsigned int line_number)
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	if (op[0] != '\0')
+	{
+		if (!isdigit(op[0]) && *ptr != '\0')
+		{
+			printf("L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	return (ret);
 }
