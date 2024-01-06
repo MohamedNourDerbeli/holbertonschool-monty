6f65c7dd16dd670bc66e0a378b71b33c91d6f315
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 14:55:33 +0100

    pall fun

---
+++ _pall.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * _pall - Prints all the values on the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the Monty bytecode file
+ **/
+void _pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	while (node != NULL)
+	{
+		printf("%d\n", node->n);
+		node = node->next;
+	}
+}
