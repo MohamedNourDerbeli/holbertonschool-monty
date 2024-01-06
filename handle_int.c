71157a842619f8778d25e4b131865626de72106b
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 18:37:47 +0100

    fix malloc

--- handle_int.c
+++ handle_int.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * _strtol - Converts a string to a long integer.
  * @op: The string to be converted.
@@ -9,24 +10,31 @@ int _strtol(char *op, unsigned int line_number)
 {
 	char *ptr;
 	long ret;
-	ret = strtol(op, &ptr, 10);
-	if (ptr == op)
+	int has_non_digit = 0;
+
+	if (*op == '\0')
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (op[0] != '\0')
+
+	ret = strtol(op, &ptr, 10);
+
+	while (*ptr != '\0')
 	{
-		if (!isdigit(op[0]) && *ptr != '\0')
+		if (!isdigit(*ptr))
 		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
+			has_non_digit = 1;
+			break;
 		}
-		if (isdigit(op[0]) && *ptr != '\0')
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
+		ptr++;
 	}
+
+	if (ptr == op || has_non_digit)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	return (ret);
 }
