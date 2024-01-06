baeb75ecd0732a0167794b58fab95c37af032168
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 20:09:48 +0100

    add

---
+++ _add.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * _add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the Monty bytecode file.
+ **/
+void _add(stack_t **stack, unsigned int line_number)
+{
+	int tmp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->n;
+	pop(stack, line_number);
+	(*stack)->n += tmp;
+}
