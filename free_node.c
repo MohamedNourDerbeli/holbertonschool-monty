0a14fe77c7b7f88035eb9e29bad69d1797011ea6
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 14:57:43 +0100

    free when exit

---
+++ free_node.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+glob_t glob;
+
+/**
+ * stack_init - initialize all the things
+ * @head: top of stack data structure
+ **/
+void stack_init(stack_t **head)
+{
+	*head = NULL;
+	glob.top = head;
+}
+/**
+ * free_all - free all malloc'ed memory
+ *     note: this is available "atexit", starting at
+ *           getline loop
+ **/
+void free_node(void)
+{
+	stack_t *tmp1, *tmp2 = NULL;
+
+	tmp1 = *(glob.top);
+	while (tmp1 != NULL)
+	{
+		tmp2 = tmp1->next;
+		free(tmp1);
+		tmp1 = tmp2;
+	}
+}
