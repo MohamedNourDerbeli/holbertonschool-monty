969963d91ce956819387602deca0b1192c376759
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 21:55:27 +0100

    fix malloc

--- handle_file.c
+++ handle_file.c
@@ -1,38 +1,46 @@
 #include "monty.h"
+
 /**
  * file - Reads and executes commands from a file.
- * @file: File name to be read.
+ * @filename: File path to be read.
  * @stack: Pointer to the top of the stack.
  * Return: EXIT_SUCCESS or EXIT_FAILURE.
  **/
 int file(char *file, stack_t **stack)
 {
-	FILE *of;
-	size_t len, ret;
-	unsigned int line_number = 0;
-	char *line = NULL;
-	char *cmd, *delim = "$\n \t\r";
+    size_t len;
+    ssize_t ret;
+    unsigned int line_number = 0;
+    char *line = NULL;
+    char *cmd, *delim = "$\n \t\r";
+    FILE *of;
 
-	if (!file)
-	{
-		fprintf(stderr, FILE_NOT_OPEN, file);
-		exit(EXIT_FAILURE);
-	}
-	of = fopen(file, "r");
-	if (of == NULL)
-	{
-		fprintf(stderr, FILE_NOT_OPEN, file);
-		exit(EXIT_FAILURE);
-	}
-	atexit(free_node);
-	while ((ret = getline(&line, &len, of) != -1))
-	{
-		cmd = strtok(line, delim);
-		line_number++;
-		if (cmd)
-			opcode(stack, cmd, line_number);
-	}
-	free(line);
-	fclose(of);
-	exit(EXIT_FAILURE);
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: File path is NULL\n");
+        exit(EXIT_FAILURE);
+    }
+
+    of = fopen(file, "r");
+    if (of == NULL)
+    {
+        fprintf(stderr, FILE_NOT_OPEN, file);
+        exit(EXIT_FAILURE);
+    }
+
+    atexit(free_node);
+
+    while ((ret = getline(&line, &len, of)) != -1)
+    {
+        line_number++;
+        cmd = strtok(line, delim);
+        if (cmd != NULL)
+        {
+            opcode(stack, cmd, line_number);
+        }
+    }
+
+    free(line);
+    fclose(of);
+    exit(EXIT_SUCCESS);
 }
