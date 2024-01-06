b6a98ec3fbbb470cd6a080c4b2a4556b3acdf112
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 21:42:38 +0100

    add case

--- _pall.c
+++ _pall.c
@@ -4,11 +4,13 @@
  * @stack: Double pointer to the top of the stack.
  * @line_number: Line number in the Monty bytecode file
  **/
-void _pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+void _pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *node = *stack;
+	stack_t *node;
+	(void)line_number;
 
-	if (stack == NULL)
+	node = *stack;
+	if (node == NULL)
 		return;
 	while (node != NULL)
 	{
