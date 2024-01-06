88df7a5b7ac61a658dd57b225bccbc4d6ea78340
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 19:01:20 +0100

    removes top element of stack

---
+++ _pop.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * _pop - Removes the top element of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the Monty bytecode file.
+ **/
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	node  = *stack;
+	(*stack) = (*stack)->next;
+	free(node);
+}
