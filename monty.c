c90b58e0a5d624d4905ac746178842a1bcb735d7
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 10:43:47 +0100

    fix return

--- monty.c
+++ monty.c
@@ -8,12 +8,10 @@
 int main(int argc, char **argv)
 {
 	stack_t *stack = NULL;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, ARGUMENT_NOT_CORRECT);
 		exit(EXIT_FAILURE);
 	}
 	file(argv[1], &stack);
-	exit(EXIT_SUCCESS);
 }
