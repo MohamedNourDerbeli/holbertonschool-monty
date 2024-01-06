571e4abd77d3c6111d389fa9ca080594fcd3aab6
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 16:13:27 +0100

    fix malloc

--- handle_int.c
+++ handle_int.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+#include <limits.h>
+
 /**
  * _strtol - Converts a string to a long integer.
  * @op: The string to be converted.
@@ -7,31 +9,35 @@
  **/
 int _strtol(char *op, unsigned int line_number)
 {
-	char *ptr;
-	long ret;
+    char *ptr;
+    long ret;
 
-	errno = 0;
-	if (op == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    errno = 0;
 
-	ret = strtol(op, &ptr, 10);
-	if ((errno == ERANGE && (ret == LONG_MAX || ret == LONG_MIN))
-				|| (errno != 0 && ret == 0))
-		exit(EXIT_FAILURE);
+    if (op[0] == '\0')
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	if (ptr == op)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	if (op[0] != '\0')
-		if (*ptr != '\0')
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-	return (ret);
+    ret = strtol(op, &ptr, 10);
+
+    if ((errno == ERANGE && (ret == LONG_MAX || ret == LONG_MIN)))
+    {
+        fprintf(stderr, "L%d: number out of range for long\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    else if (errno != 0 && ret == 0)
+    {
+        perror("strtol");
+        exit(EXIT_FAILURE);
+    }
+
+    if (*ptr != '\0')
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    return (ret);
 }
