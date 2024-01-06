7e351525393f12d2415cf46fb95f017a2c9c0f0c
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 19:08:11 +0100

    removes top element of stack

--- _pop.c
+++ _pop.c
@@ -8,7 +8,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node;
 
-	if (stack == NULL || *stack == NULL)
+	if (*stack == NULL)
 	{
 		printf("L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
