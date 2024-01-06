54caab47f37714a9179db8da34d0e7d168f4cc5f
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-06 22:45:14 +0100

    add

--- _add.c
+++ _add.c
@@ -8,12 +8,14 @@ void _add(stack_t **stack, unsigned int line_number)
 {
 	int sum;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	sum = (*stack)->n + (*stack)->next->n;
 	(*stack)->next->n = sum;
-	_pop(stack, line_number);
+	(*stack) = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 }
