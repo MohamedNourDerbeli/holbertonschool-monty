70fa8097494fd1e4fa24126205f07537d49ef6a9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-09 19:55:02 +0100

    add the _mul

--- _mul.c
+++ _mul.c
@@ -13,7 +13,11 @@ void _mul(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d:  multiplies by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	sum = (*stack)->n % (*stack)->next->n;
 	(*stack)->next->n = sum;
 	(*stack) = (*stack)->next;
